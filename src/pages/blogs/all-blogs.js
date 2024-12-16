62fdfa292d0dff7f25165983e10c7b32dce6c0b0
David_Velez <velezdavid6@gmail.com>
2024-12-16 16:04:47 -0500

    worked on the all blogs page

--- src/pages/blogs/all-blogs.js
+++ src/pages/blogs/all-blogs.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { Link, graphql } from "gatsby";
 import "../../styles/blogs.css";
+import { GatsbyImage } from "gatsby-plugin-image"; // Import GatsbyImage
 import { useTheme } from "../../utils/ThemeContext";
 import RotateWarning from "../../components/RotateWarning";
 import ThemeToggler from "../../components/ThemeToggler";
 import arrow from "../../images/arrow-icon.svg";
 import arrowLight from "../../images/arrow-icon-light.svg";
+import link from "../../images/link-arrow.svg";
+import lightLink from "../../images/light-link-arrow.svg";
 
 const AllBlogs = ({ data }) => {
   const { theme, toggleTheme } = useTheme();
@@ -63,13 +66,134 @@ const AllBlogs = ({ data }) => {
           <ThemeToggler theme={theme} toggleTheme={toggleTheme} />
         </div>
         <div className="blogs-container">
-          <ul>
-            {blogs.map(({ node }) => (
-              <li key={node.slug}>
-                <Link to={`/blogs/${node.slug}`}>{node.title}</Link>
-              </li>
-            ))}
-          </ul>
+          {blogs.map(({ node }) => (
+            // <li key={node.slug}>
+            //   <Link to={`/blogs/${node.slug}`}>{node.title}</Link>
+            // </li>
+            <div
+              key={node.slug}
+              className="blog-item-container bottom-spacing"
+              data-theme={theme}
+            >
+              <div className="mobile-info-container bottom-spacing">
+                <span
+                  style={{
+                    flex: pageTitle ? "1" : "none",
+                    transition: "flex 0.5s ease-in-out",
+                  }}
+                  className={
+                    theme === "dark"
+                      ? "heading-line-reversed"
+                      : "light-heading-line"
+                  }
+                ></span>
+                <Link
+                  to={`/blogs/${node.slug}`}
+                  className="secondary-heading-semi-bold no-decoration"
+                  data-theme={theme}
+                >
+                  Read Article
+                </Link>
+                {/* <img
+                  className="link-arrow"
+                  src={theme === "dark" ? link : lightLink}
+                  alt="some alt text"
+                /> */}
+              </div>
+              {/* <div className="preview-image-container bottom-spacing">
+                <GatsbyImage
+                  image={node.headerImage.gatsbyImageData}
+                  alt={node.title || "Blog Image"} // Add alt text
+                  className="preview-image"
+                />
+              </div> */}
+              {/* <div className="mobile-info-container bottom-spacing">
+                <p className="copy-font" data-theme={theme}>
+                  {node.date}
+                </p>
+                <p className="copy-font" data-theme={theme}>
+                  by: {node.author}
+                </p>
+              </div> */}
+              <div className="blog-title-container bottom-spacing">
+                {/* <div className="preview-image-container">
+                  <GatsbyImage
+                    image={node.headerImage.gatsbyImageData}
+                    alt={node.title || "Blog Image"} // Add alt text
+                    className="preview-image"
+                  />
+                </div> */}
+                <div
+                  className={
+                    theme === "dark"
+                      ? "preview-image-container"
+                      : "light-preview-image-container"
+                  }
+                >
+                  {/* <img
+                    style={{
+                      boxShadow:
+                        theme === "dark" ? "none" : "8px 5px 15px #555151",
+                    }}
+                    className="project-image"
+                    src={panasonic}
+                    alt="some alt text"
+                  /> */}
+                  <GatsbyImage
+                    image={node.headerImage.gatsbyImageData}
+                    alt={node.title || "Blog Image"} // Add alt text
+                    className="preview-image"
+                    style={{
+                      boxShadow:
+                        theme === "dark" ? "none" : "4px 7px 7px rgba(85, 81, 81, 0.6)",
+                    }}
+                  />
+                </div>
+                <p className="primary-heading-bold" data-theme={theme}>
+                  {node.title}
+                </p>
+                {/* <Link to={`/blogs/${node.slug}`}>{node.title}</Link> */}
+              </div>
+              <div className="mobile-info-container bottom-spacing">
+                <p className="copy-font" data-theme={theme}>
+                  published: {node.date}
+                </p>
+              </div>
+              <div className="blog-tags-container">
+                {node.tags && node.tags.length > 0 ? (
+                  node.tags.map((item, index) => (
+                    <div key={index} className="tag" data-theme={theme}>
+                      {item.tag}
+                    </div>
+                  ))
+                ) : (
+                  <div className="blog-tags-container">
+                    <p className="copy-font" data-theme={theme}>
+                      No tags available
+                    </p>
+                  </div>
+                )}
+                {/* <div className="tag" data-theme={theme}>
+                  ELECTRONICS
+                </div>
+                <div className="tag" data-theme={theme}>
+                  E-COMMERCE
+                </div>
+                <div className="tag" data-theme={theme}>
+                  CUSTOM
+                </div>
+                <div className="tag" data-theme={theme}>
+                  SHOPIFY
+                </div>
+                <div className="tag" data-theme={theme}>
+                  UX/UI
+                </div>
+                <div className="tag" data-theme={theme}>
+                  ACCESSIBILITY
+                </div> */}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
@@ -81,9 +205,16 @@ export const query = graphql`
     allContentfulArticle(sort: { fields: [date], order: DESC }) {
       edges {
         node {
+          headerImage {
+            gatsbyImageData
+          }
           slug
           title
-          date(formatString: "MMMM DD, YYYY")
+          author
+          date(formatString: "MMM DD, 'YY")
+          tags {
+            tag
+          }
         }
       }
     }
